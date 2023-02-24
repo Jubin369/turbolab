2717dc0dd42b6e1baa2db03e9159c675d36ff2b4
jubin369 <jubinayoob369@gmail.com>
2023-02-24 16:16:35 +0530

    shows rick and morty data...

--- rickandmorty-react-graphql/src/App.js
+++ rickandmorty-react-graphql/src/App.js
@@ -1,7 +1,27 @@
-const App = () => (
-  <div className="App">
-    <h1>Hello world</h1>
-  </div>
-);
+import { useQuery } from "@apollo/client";
+import { GET_ALL_CHARACTERS } from "./queries";
+import Card from "./Card";
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_ALL_CHARACTERS, {
+    variables: { page: 3 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error {error.message}</p>;
+
+  return (
+    <>
+      <header>
+        <h1 className="text-center m-5">Rick and Morty GraphQL App</h1>
+      </header>
+      <div className="row">
+        {data?.characters?.results.map((character) => (
+          <Card character={character} key={character.id} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default App;
