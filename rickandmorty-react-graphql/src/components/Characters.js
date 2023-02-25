f388a26bb82d5be9ef9df5e3cffd93b647988ed4
jubin369 <jubinayoob369@gmail.com>
2023-02-25 16:00:42 +0530

    changes

--- rickandmorty-react-graphql/src/components/Characters.js
+++ rickandmorty-react-graphql/src/components/Characters.js
@@ -32,7 +32,6 @@ const Characters = () => {
     getSearchCharaters();
   }, [searchQuery]);
 
-  //if (loading) return <p>Loading...</p>;
   if (error) return <p>Error {error.message}</p>;
 
   // const isBottomNavigation =
