c6f88a63487901b3ef779867631d131c797bb70d
jubin369 <jubinayoob369@gmail.com>
2023-02-25 10:14:13 +0530

    adds chakra-ui packages

--- rickandmorty-react-graphql/src/index.js
+++ rickandmorty-react-graphql/src/index.js
@@ -3,6 +3,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import theme from "./styles/theme";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URL,
@@ -12,8 +14,10 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <ChakraProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </ChakraProvider>
   </BrowserRouter>
 );
