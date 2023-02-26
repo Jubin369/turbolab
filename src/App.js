a287a99268312751f76d7434ed587018145ef0b0
jubin369 <jubinayoob369@gmail.com>
2023-02-26 14:50:42 +0530

    shows episode details page...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Characters from "./components/Characters";
 import Episodes from "./components/Episodes";
 import CharacterDetails from "./components/CharacterDetails";
+import EpisodeDetails from "./components/EpisodeDetails";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Characters />} />
         <Route path="/episodes" element={<Episodes />} />
         <Route path="/character/:id" element={<CharacterDetails />} />
+        <Route path="/episode/:id" element={<EpisodeDetails />} />
       </Routes>
     </>
   );
---
+++ src/components/EpisodeDetails.js
@@ -0,0 +1,68 @@
+import { useQuery } from "@apollo/client";
+import { Image, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { GET_EPISODE } from "../queries";
+
+const EpisodeDetails = () => {
+  const { id } = useParams();
+
+  const { loading, error, data } = useQuery(GET_EPISODE, {
+    variables: { id: id },
+  });
+
+  let episode = data?.episode;
+
+  if (error) return <p>Error {error.message}</p>;
+
+  return (
+    <Stack backgroundColor="skyblue">
+      {" "}
+      <SimpleGrid
+        columns={[1, 1, 2, 2]}
+        w={["100%", "100%", "80%", "60%"]}
+        spacing={5}
+        px="20px"
+        mt="30px"
+        alignSelf={"center"}
+        backgroundColor="blue.100"
+      >
+        <Stack borderRadius="10px">
+          <Stack p="20px">
+            <Text fontWeight={"bold"}>{episode?.name}</Text>
+
+            <Stack direction="row">
+              <Text fontSize={"sm"} color="GrayText">
+                Episode:
+              </Text>
+              <Text fontSize={"sm"}>{episode?.episode}</Text>
+            </Stack>
+            <Stack direction="row">
+              <Text fontSize={"sm"} color="GrayText">
+                Air date:
+              </Text>
+              <Text fontSize={"sm"}>{episode?.air_date}</Text>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack>
+          <Text fontWeight={"bold"} my="30px" textAlign="center">
+            Characters in Episode
+          </Text>
+          {episode?.characters?.map((character) => {
+            return (
+              <a href={`/character/${character?.id}`}>
+                <Stack backgroundColor={"blue.300"} borderRadius="5px" p="10px">
+                  <Text>name: {character.name}</Text>
+                </Stack>
+              </a>
+            );
+          })}
+        </Stack>
+      </SimpleGrid>
+    </Stack>
+  );
+};
+
+export default EpisodeDetails;
